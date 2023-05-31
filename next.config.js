fada5503f89f2e19d471fbda399634a43f95c022
Jan Böhm <4008395+J-Boehm@users.noreply.github.com>
2023-05-31 20:15:04 +0200

    Add babel config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withTwin = require("./withTwin");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withTwin(nextConfig);
