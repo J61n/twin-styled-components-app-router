3e6d57851f0929aeb8285bd0ba3ea076c020b89c
Jan Böhm <4008395+J-Boehm@users.noreply.github.com>
2023-06-01 22:00:59 +0200

    Construct example case

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import tw from "twin.macro";
+
+export const Button = tw.button`
+  bg-pink-500 text-pink-50 hover:bg-pink-400 active:bg-pink-600
+  border border-pink-600 hover:border-pink-500
+  font-medium text-lg
+  px-5 py-2
+  rounded shadow
+`;
